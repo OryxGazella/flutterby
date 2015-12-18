60c1a652c170cd450392c97eff082748ca95d591
Frank Smith <oryx.gazella@gmail.com>
2015-12-18 23:14:34 +0200

    Add babel transpiler for ES2015 features.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 module.exports = {
-    entry: "./main.es6",
+    entry: "./main.jsx",
     output: {
         path: __dirname,
         filename: "index.js"
     },
     module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
     }
 };
