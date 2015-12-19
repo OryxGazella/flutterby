0d4f112550e9deb7ee97e4a9f2a0a700491d4f65
Frank Smith <oryx.gazella@gmail.com>
2015-12-19 16:12:57 +0200

    Add .editorconfig and set indentation of 2 chars, apply globally.

--- usercontrols.js
+++ usercontrols.js
@@ -1,12 +1,12 @@
 const Mousetrap = require('mousetrap')
 
 function bindKey(key) {
-    //noinspection JSClosureCompilerSyntax
-    let sub = new Rx.Subject()
-    Mousetrap.bind(key, () => {
-        sub.onNext(key)
-    })
-    return sub
+  //noinspection JSClosureCompilerSyntax
+  let sub = new Rx.Subject()
+  Mousetrap.bind(key, () => {
+    sub.onNext(key)
+  })
+  return sub
 }
 
 const UserControlStream = Rx.Observable.merge(bindKey('space'), bindKey('left'), bindKey('right'), bindKey('down'), bindKey('up'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-    entry: "./main.jsx",
-    output: {
-        path: __dirname,
-        filename: "index.js"
-    },
-    devServer: {
-        inline: true,
-        port: 8080
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    }
+  entry: "./main.jsx",
+  output: {
+    path: __dirname,
+    filename: "index.js"
+  },
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
 };
